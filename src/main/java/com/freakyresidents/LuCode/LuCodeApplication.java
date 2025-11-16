8f0e8803bf066aac9c84151694b6a09e06d9ddd5
Nyan Min Htet <nyanminhtet2304@gmail.com>
2025-11-16 12:51:23 +0630

    Tech stack change

---
+++ src/main/java/com/freakyresidents/LuCode/LuCodeApplication.java
@@ -0,0 +1,13 @@
+package com.freakyresidents.LuCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuCodeApplication.class, args);
+	}
+
+}
